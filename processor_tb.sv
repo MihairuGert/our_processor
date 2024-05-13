c44fc5d287c2ff2483f6708d8f7dbe86511069ef
mihairu <121375098+MihairuGert@users.noreply.github.com>
2024-05-13 17:50:33 +0700

    Update processor_tb.sv

--- processor_tb.sv
+++ processor_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.05.2024 12:59:15
-// Design Name: 
-// Module Name: processor_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module processor_tb();
 logic clk, reset;
